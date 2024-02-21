6b2cb39985c7f1320b9c94221134e00698cb128e
MelvinCureau <melvin.cureau2004@gmail.com>
2024-02-21 11:43:29 +0100

    up

--- sqlDatabase/istoreapp.sql
+++ sqlDatabase/istoreapp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Dim 28 Janvier 2024 à 18:30
+-- Généré le :  Mer 21 Février 2024 à 11:36
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -19,33 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `istoreapp`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `admins`
---
-
-CREATE TABLE IF NOT EXISTS `admins` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `pseudo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `employees`
---
-
-CREATE TABLE IF NOT EXISTS `employees` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pseudo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `store_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -70,9 +43,16 @@ CREATE TABLE IF NOT EXISTS `items` (
   `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` double NOT NULL,
   `quantity` int(11) NOT NULL,
-  `inventory_id` int(11) NOT NULL,
+  `store` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `price`, `quantity`, `store`) VALUES
+(3, 'article1', 12.99, 4, 'mag1');
 
 -- --------------------------------------------------------
 
@@ -84,7 +64,14 @@ CREATE TABLE IF NOT EXISTS `stores` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `stores`
+--
+
+INSERT INTO `stores` (`id`, `name`) VALUES
+(4, 'mag1');
 
 -- --------------------------------------------------------
 
@@ -99,14 +86,35 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=11 ;
 
 --
 -- Contenu de la table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `pseudo`, `password`, `role`) VALUES
-(1, 'melvin.cureau@test.com', 'melvin.cureau', 'azerty', 'pdg');
+(10, 'admin', 'admin', '$2a$10$JxmaWMuQenNdK1Mp42OfCeaRJ3g6AKkNXXFGM33uvPvrl7msCQw0S', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `whitelist`
+--
+
+CREATE TABLE IF NOT EXISTS `whitelist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `whitelist`
+--
+
+INSERT INTO `whitelist` (`id`, `email`) VALUES
+(4, 'admin'),
+(5, 'user@user.com'),
+(6, '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
